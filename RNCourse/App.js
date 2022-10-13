444d11714ecae9f8bfedb7cbd09777635ced34fb
Jan Ranostaj <HonzaRanostaj@gmail.com>
2022-10-13 21:50:47 +0200

    14

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Button, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View>
+        <Text>Another piece of text</Text>
+      </View>
       <Text>Hello world!</Text>
-      <StatusBar style="auto" />
+      <Button title="tap me!" />
     </View>
   );
 }
