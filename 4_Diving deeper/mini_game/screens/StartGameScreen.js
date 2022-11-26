29785dccdc32c92d375579ae505d713ff1fba467
Jan Ranostaj <HonzaRanostaj@gmail.com>
2022-11-26 21:48:06 +0100

    50

--- 4_Diving deeper/mini_game/screens/StartGameScreen.js
+++ 4_Diving deeper/mini_game/screens/StartGameScreen.js
@@ -5,7 +5,7 @@ import PrimaryButton from "../components/PrimaryButton";
 const StartGameScreen = () => {
     return (
         <View style={styles.inputContainer}>
-            <TextInput/>
+            <TextInput style={styles.numberInput} maxLength={2}/>
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
@@ -15,19 +15,30 @@ const StartGameScreen = () => {
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-   inputContainer: {
-       padding: 16,
-       marginTop: 100,
-       marginHorizontal: 24,
-       borderRadius: 8,
-       backgroundColor: '#72063c',
-       elevation: 4,
-       shadowColor: 'black',
-       shadowOffset: {
-           width: 2,
-           height:2,
-       },
-       shadowRadius: 6,
-       shadowOpacity: 0.25
-   }
+    inputContainer: {
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        borderRadius: 8,
+        backgroundColor: '#72063c',
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: {
+            width: 2,
+            height: 2,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    },
+    numberInput: {
+        height: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: "bold",
+        width: 50,
+        textAlign: 'center'
+    }
 });
