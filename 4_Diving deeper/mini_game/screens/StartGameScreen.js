f614fe8e1036e2edfcdb60e2d9e5530710a104ab
Jan Ranostaj <HonzaRanostaj@gmail.com>
2022-11-26 21:38:45 +0100

    48

--- 4_Diving deeper/mini_game/screens/StartGameScreen.js
+++ 4_Diving deeper/mini_game/screens/StartGameScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {TextInput, View} from "react-native";
+import {StyleSheet, TextInput, View} from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
     return (
-        <View>
+        <View style={styles.inputContainer}>
             <TextInput/>
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
@@ -13,3 +13,21 @@ const StartGameScreen = () => {
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+   inputContainer: {
+       padding: 16,
+       marginTop: 100,
+       marginHorizontal: 24,
+       borderRadius: 8,
+       backgroundColor: '#72063c',
+       elevation: 4,
+       shadowColor: 'black',
+       shadowOffset: {
+           width: 2,
+           height:2,
+       },
+       shadowRadius: 6,
+       shadowOpacity: 0.25
+   }
+});
