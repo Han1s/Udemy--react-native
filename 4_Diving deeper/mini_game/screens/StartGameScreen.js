3f6319e44387947080e34dd103c5f609fb07bd54
Jan Ranostaj <HonzaRanostaj@gmail.com>
2022-11-26 16:55:37 +0100

    48

--- 4_Diving deeper/mini_game/screens/StartGameScreen.js
+++ 4_Diving deeper/mini_game/screens/StartGameScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {TextInput, View} from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
     return (
         <View>
             <TextInput/>
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
         </View>
     );
 };
