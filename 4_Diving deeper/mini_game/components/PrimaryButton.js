8046e52087f48f88cf311bac9e7077f1b2d74959
Jan Ranostaj <HonzaRanostaj@gmail.com>
2022-11-27 12:41:27 +0100

    52

--- 4_Diving deeper/mini_game/components/PrimaryButton.js
+++ 4_Diving deeper/mini_game/components/PrimaryButton.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {Pressable, Text, View, StyleSheet} from "react-native";
 
 const PrimaryButton = ({children}) => {
+    const pressHandler = () => {
+        console.log('pressed');
+    }
+
     return (
-        <View>
-            <Text>{children}</Text>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                onPress={pressHandler}
+                style={({pressed}) => pressed
+                    ? [styles.buttonInnerContainer, styles.pressed]
+                    : styles.buttonInnerContainer}
+                android_ripple={{color: "#640233"}}>
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
         </View>
     );
 };
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden'
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#72063c',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    },
+    pressed: {  // IOS solution
+        opacity: 0.75
+    }
+})
