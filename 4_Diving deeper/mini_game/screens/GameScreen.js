86d5abfda07818a6321e0c1b524aee0fcf582555
Jan Ranostaj <HonzaRanostaj@gmail.com>
2022-11-28 21:27:53 +0100

    62

--- 4_Diving deeper/mini_game/screens/GameScreen.js
+++ 4_Diving deeper/mini_game/screens/GameScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {StyleSheet, Text, View} from "react-native";
+import Title from "../components/Title";
 
 const GameScreen = () => {
     return (
         <View style={styles.screen}>
-            <Text>Oponent's Guess</Text>
+            <Title>Oponent's Guess</Title>
             {/*GUESS*/}
             <View>
                 <Text>Higher or lower?</Text>
