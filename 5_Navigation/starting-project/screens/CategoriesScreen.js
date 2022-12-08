c9a1bc9a8a298872a60fd836615ac4a2ac6c987d
Jan Ranostaj <HonzaRanostaj@gmail.com>
2022-12-08 16:11:52 +0100

    92

--- 5_Navigation/starting-project/screens/CategoriesScreen.js
+++ 5_Navigation/starting-project/screens/CategoriesScreen.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 import { CATEGORIES } from "../data/dummy-data";
 import {FlatList} from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
 
 const CategoriesScreen = () => {
 
-    const renderCategoryItem = (item) => {
+    const renderCategoryItem = (itemData) => {
         return (
-
+            <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
         )
     }
 
     return (
         <FlatList
             data={CATEGORIES}
-            renderItem={}
+            renderItem={renderCategoryItem}
             keyExtractor={item => item.id} />
     );
 };
