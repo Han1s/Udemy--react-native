fd97d1ca4188d958f02ccdb6512ea2e49f7ef9fe
Jan Ranostaj <HonzaRanostaj@gmail.com>
2022-12-09 16:27:00 +0100

    99

--- 5_Navigation/starting-project/screens/MealsOverviewScreen.js
+++ 5_Navigation/starting-project/screens/MealsOverviewScreen.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { MEALS } from "../data/dummy-data";
-import {Text, View, StyleSheet} from "react-native";
+import {Text, View, StyleSheet, FlatList} from "react-native";
+import MealItem from "../components/MealItem";
 
 const MealsOverviewScreen = ({ route }) => {
     const { categoryId } = route.params;
 
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.includes(categoryId)
+    })
+
+    const renderMealItem = (itemData) => <MealItem title={itemData.item.title} />
+
     return (
         <View style={styles.container}>
-            <Text>Meals Overview Screen = {categoryId}</Text>
+            <FlatList
+                keyExtractor={(item) => item.id}
+                data={displayedMeals}
+                renderItem={renderMealItem}
+            />
         </View>
     );
 };
