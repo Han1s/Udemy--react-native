621d611c9d5ecc034fa3a0cc86b8a9b6782ad43d
Jan Ranostaj <HonzaRanostaj@gmail.com>
2023-01-05 16:51:42 +0100

    11

--- 5_Navigation/starting-project/App.js
+++ 5_Navigation/starting-project/App.js
@@ -1,49 +1,48 @@
 import {StatusBar} from 'expo-status-bar';
-import {Button, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import CategoriesScreen from "./screens/CategoriesScreen";
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import {createDrawerNavigator} from '@react-navigation/drawer'
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator()
+
+const DrawerNavigator = () => {
+    return (
+        <Drawer.Navigator screenOptions={{
+            headerStyle: {backgroundColor: '#351401'},
+            headerTintColor: 'white',
+            sceneContainerStyle: {backgroundColor: '#3f2f25'}
+        }}>
+            <Drawer.Screen component={CategoriesScreen} name={'Categories'} options={{
+                title: 'All Categories'
+            }}/>
+            <Drawer.Screen component={FavoritesScreen} name={'Favorites'}/>
+        </Drawer.Navigator>
+    )
+};
 
 export default function App() {
     return (
         <>
             <StatusBar style={'light'}/>
             <NavigationContainer>
-                <Stack.Navigator screenOptions={{
-                    headerStyle: {backgroundColor: '#351401'},
-                    headerTintColor: 'white',
-                    contentStyle: {
-                        backgroundColor: '#3f2f25'
-                    }
-                }}>
+                <Stack.Navigator>
                     <Stack.Screen
-                        options={{
-                            title: 'All Categories',
-                            // headerStyle: {backgroundColor: '#351401'},
-                            // headerTintColor: 'white',
-                            // contentStyle: {
-                            //     backgroundColor: '#351401'
-                            // }
-                        }}
-                        name="MealsCategories"
-                        component={CategoriesScreen}/>
+                        options={{headerShown: false}}
+                        name="Drawer"
+                        component={DrawerNavigator}/>
                     <Stack.Screen
                         name="MealsOverview"
                         component={MealsOverviewScreen}
                     />
                     <Stack.Screen name={'MealDetail'}
                                   component={MealDetailScreen}
-                                  // options={{
-                                  //     headerRight: () => {
-                                  //         return (
-                                  //             <Button title={'Tap me!'} onPress={}/>
-                                  //         )
-                                  //     }
-                                  // }}
+                                  options={{title: 'About the Meal'}}
                     />
                 </Stack.Navigator>
             </NavigationContainer>
---
+++ 5_Navigation/starting-project/screens/FavoritesScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from "react-native";
+
+const FavoritesScreen = () => {
+    return (
+        <Text>
+            Favorites screen!
+        </Text>
+    );
+};
+
+export default FavoritesScreen;
