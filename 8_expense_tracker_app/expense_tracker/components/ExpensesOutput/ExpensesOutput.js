bcbd4b52fc8a5d2bf614b7734e9918c90a75cba6
Jan Ranostaj <HonzaRanostaj@gmail.com>
2023-01-13 16:29:11 +0100

    135

--- 8_expense_tracker_app/expense_tracker/components/ExpensesOutput/ExpensesOutput.js
+++ 8_expense_tracker_app/expense_tracker/components/ExpensesOutput/ExpensesOutput.js
@@ -22,19 +22,19 @@ const DUMMY_EXPENSES = [
     id: "e3",
     description: "Some bananas",
     amount: 5.99,
-    date: new Date("2021-12-1"),
+    date: new Date("2021-12-01"),
   },
   {
     id: "e4",
     description: "A book",
     amount: 14.99,
-    date: new Date("2022-19-2"),
+    date: new Date("2022-02-19"),
   },
   {
     id: "e5",
     description: "Another book",
     amount: 18.59,
-    date: new Date("2022-18-2"),
+    date: new Date("2022-02-19"),
   },
       {
     id: "e11",
