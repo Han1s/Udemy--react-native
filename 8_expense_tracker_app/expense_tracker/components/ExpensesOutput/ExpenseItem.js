b3ef6fe1e9c897d2ceea84862fc8a3c4af44e1d3
Jan Ranostaj <HonzaRanostaj@gmail.com>
2023-01-13 16:23:13 +0100

    133

--- 8_expense_tracker_app/expense_tracker/components/ExpensesOutput/ExpenseItem.js
+++ 8_expense_tracker_app/expense_tracker/components/ExpensesOutput/ExpenseItem.js
@@ -5,13 +5,13 @@ import {GlobalStyles} from "../../constants/styles";
 const ExpenseItem = ({ description, amount, date }) => {
   return (
     <Pressable>
-      <View>
+      <View style={styles.expenseItem}>
         <View>
-          <Text>{description}</Text>
-          <Text>{date}</Text>
+          <Text style={[styles.textBase, styles.description]}>{description}</Text>
+          <Text style={styles.textBase}>{date.toString()}</Text>
         </View>
-        <View>
-          <Text>{amount}</Text>
+        <View style={styles.amountContainer}>
+          <Text style={styles.amount}>{amount}</Text>
         </View>
       </View>
     </Pressable>
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     expenseItem: {
         padding: 12,
         marginVertical: 8,
-        backgroundColor: GlobalStyles.colors.primary500
+        backgroundColor: GlobalStyles.colors.primary500,
         flexDirection: 'row',
         justifyContent: 'space-between',
         borderRadius: 6,
@@ -37,4 +37,24 @@ const styles = StyleSheet.create({
         },
         shadowOpacity: 0.4
     },
+    textBase: {
+        color: GlobalStyles.colors.primary50
+    },
+    description: {
+        fontSize: 16,
+        marginBottom: 4,
+        fontWeight: 'bold'
+    },
+    amountContainer: {
+        paddingHorizontal: 12,
+        paddingVertical: 4,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4
+    },
+    amount: {
+        color: GlobalStyles.colors.primary500,
+        fontWeight: 'bold'
+    }
 });
