d64a34d3de76ce90e1bda42477ab6cf5ef13c24c
Jan Ranostaj <HonzaRanostaj@gmail.com>
2023-01-29 12:47:21 +0100

    148

---
+++ 8_expense_tracker_app/expense_tracker/components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View } from "react-native";
+import Input from "./Input";
+
+const ExpenseForm = () => {
+  const amountChangedHandler = (text) => {};
+
+  return (
+    <View>
+      <Input
+        label={"Amount"}
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          placeholder: "0.00",
+          onChangeText: amountChangedHandler,
+        }}
+      />
+      <Input
+        label={"Date"}
+        textInputConfig={{
+          keyboardType: "number-pad",
+          placeholder: "YYYY-MM-DD",
+          maxLength: 10,
+          onChangeText: () => {},
+        }}
+      />
+      <Input label={"Description"} textInputConfig={{}} />
+    </View>
+  );
+};
+
+export default ExpenseForm;
