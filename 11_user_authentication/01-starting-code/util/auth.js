254e649fb3a58ed044d77aaab6f31a86c98a4745
Jan Ranostaj <HonzaRanostaj@gmail.com>
2023-02-02 16:17:48 +0100

    177

--- 11_user_authentication/01-starting-code/util/auth.js
+++ 11_user_authentication/01-starting-code/util/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyB8Uws7QwbAqjpgie8qTxNbetJ2k94zmgc";
 
-const createUser = async (email, password) => {
+export const createUser = async (email, password) => {
     const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`, {
         email: email,
         password: password,
