53fc2b25fe88ee63001a57c6659c9121089d8f85
Jan Ranostaj <honzaranostaj@gmail.com>
2023-03-17 16:23:26 +0100

    199

--- 12_native_features/components/Places/ImagePicker.js
+++ 12_native_features/components/Places/ImagePicker.js
@@ -6,6 +6,7 @@ import {
   PermissionStatus,
 } from "expo-image-picker";
 import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
 
 const ImagePicker = () => {
   const [pickedImage, setPickedImage] = useState("");
@@ -54,7 +55,9 @@ const ImagePicker = () => {
   return (
     <View>
       <View style={styles.imagePreview}>{imagePreview}</View>
-      <Button title={"Take Image"} onPress={takeImageHandler} />
+      <OutlinedButton icon={"camera"} onPress={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
     </View>
   );
 };
