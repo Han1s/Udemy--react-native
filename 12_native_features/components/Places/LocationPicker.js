1842aecb4be0d9ff13d05c09cad2309317bf152e
Jan Ranostaj <honzaranostaj@gmail.com>
2023-03-17 17:49:07 +0100

    202

--- 12_native_features/components/Places/LocationPicker.js
+++ 12_native_features/components/Places/LocationPicker.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Alert, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Image, StyleSheet, View, Text } from "react-native";
 import OutlinedButton from "../UI/OutlinedButton";
 import { Colors } from "../../constants/colors";
 import {
@@ -8,8 +8,10 @@ import {
   PermissionStatus,
   Accuracy,
 } from "expo-location";
+import { getMapPreview } from "../../util/location";
 
 const LocationPicker = () => {
+  const [pickedLocation, setPickedLocation] = useState();
   const [locationPermissionInfo, requestPermission] =
     useForegroundPermissions();
   const verifyPermissions = async () => {
@@ -38,14 +40,29 @@ const LocationPicker = () => {
     const location = await getCurrentPositionAsync({
       accuracy: Accuracy.Highest,
     });
-    console.log(location);
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
   };
 
   const pickOnMapHandler = () => {};
 
+  let locationPreview = <Text>No location picked yet.</Text>;
+  if (pickedLocation) {
+    locationPreview = (
+      <Image
+        style={styles.image}
+        source={{
+          uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
+        }}
+      />
+    );
+  }
+
   return (
     <View>
-      <View style={styles.mapPreview}></View>
+      <View style={styles.mapPreview}>{locationPreview}</View>
       <View style={styles.actions}>
         <OutlinedButton icon={"location"} onPress={getLocationHandler}>
           Locate User
@@ -74,5 +91,9 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
