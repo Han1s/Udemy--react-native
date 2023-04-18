32313403abbfc32d9451f30a4a36570cc51a1d64
Jan Ranostaj <HonzaRanostaj@gmail.com>
2023-04-18 21:09:35 +0200

    210

--- 12_native_features/components/Places/PlaceForm.js
+++ 12_native_features/components/Places/PlaceForm.js
@@ -2,11 +2,12 @@ import { useCallback, useState } from "react";
 import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
 import { Colors } from "../../constants/colors";
+import { Place } from "../../models/place";
 import ImagePicker from "./ImagePicker";
 import LocationPicker from "./LocationPicker";
 import Button from "../UI/Button";
 
-function PlaceForm() {
+function PlaceForm({onCreatePlace}) {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [pickedLocation, setPickedLocation] = useState();
   const [selectedImage, setSelectedImage] = useState();
@@ -24,9 +25,8 @@ function PlaceForm() {
   }, []);
 
   const savePlaceHandler = () => {
-    console.log(enteredTitle);
-    console.log(selectedImage);
-    console.log(pickedLocation);
+    const placeData = new Place(enteredTitle, selectedImage, pickedLocation);;
+    onCreatePlace(placeData);
   };
 
   return (
--- 12_native_features/screens/AddPlace.js
+++ 12_native_features/screens/AddPlace.js
@@ -1,7 +1,13 @@
 import PlaceForm from "../components/Places/PlaceForm";
 
-const AddPlace = () => {
-  return <PlaceForm />;
+const AddPlace = ({ navigation }) => {
+  const createPlaceHandler = (place) => {
+    navigation.navigatie("AllPlaces", {
+      place,
+    });
+  };
+
+  return <PlaceForm onCreatePlace={createPlaceHandler} />;
 };
 
 export default AddPlace;
