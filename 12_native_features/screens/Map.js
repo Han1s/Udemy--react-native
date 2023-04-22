6d123f17c4e20f5155968a7b89f7087898456bb4
Jan Ranostaj <HonzaRanostaj@gmail.com>
2023-04-22 11:40:45 +0200

    219

--- 12_native_features/screens/Map.js
+++ 12_native_features/screens/Map.js
@@ -1,62 +1,88 @@
-import {useCallback, useLayoutEffect, useState} from 'react';
-import {Alert, StyleSheet} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import { useCallback, useLayoutEffect, useState } from "react";
+import { Alert, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 import IconButton from "../components/UI/IconButton";
 
-function Map({navigation}) {
-    const [selectedLocation, setSelectedLocation] = useState();
+function Map({ navigation, route }) {
+  const initialLocation = route.params && {
+    lat: route.params.initialLat,
+    lng: route.params.initialLng,
+  };
 
-    const region = {
-        latitude: 37.78,
-        longitude: -122.43,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-    };
+  const [selectedLocation, setSelectedLocation] = useState(initialLocation);
 
-    function selectLocationHandler(event) {
-        const lat = event.nativeEvent.coordinate.latitude;
-        const lng = event.nativeEvent.coordinate.longitude;
+  const region = {
+    latitude: initialLocation ? initialLocation.lat : 37.78,
+    longitude: initialLocation ? initialLocation.lng : -122.43,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
 
-        setSelectedLocation({lat, lng});
+  function selectLocationHandler(event) {
+    if (initialLocation) {
+      return;
     }
 
-    const savePickedLocationHandler = useCallback(() => {
-        if (!selectedLocation) {
-            Alert.alert('No location picked!', 'You have to pick a location by tapping on the map first!');
-        } else {
-            navigation.navigate('AddPlace', {pickedLat: selectedLocation.lat, pickedLng: selectedLocation.lng});
-        }
-    }, [navigation, selectedLocation]);
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerRight: ({tintColor}) => <IconButton size={24} color={tintColor} onPress={savePickedLocationHandler} icon={"save"}/>
-        })
-    }, [navigation, savePickedLocationHandler]);
+    setSelectedLocation({ lat, lng });
+  }
 
-    return (
-        <MapView
-            style={styles.map}
-            initialRegion={region}
-            onPress={selectLocationHandler}
-        >
-            {selectedLocation && (
-                <Marker
-                    title="Picked Location"
-                    coordinate={{
-                        latitude: selectedLocation.lat,
-                        longitude: selectedLocation.lng,
-                    }}
-                />
-            )}
-        </MapView>
-    );
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert(
+        "No location picked!",
+        "You have to pick a location by tapping on the map first!"
+      );
+    } else {
+      navigation.navigate("AddPlace", {
+        pickedLat: selectedLocation.lat,
+        pickedLng: selectedLocation.lng,
+      });
+    }
+  }, [navigation, selectedLocation]);
+
+  useLayoutEffect(() => {
+    if (initialLocation) {
+      return;
+    }
+
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          size={24}
+          color={tintColor}
+          onPress={savePickedLocationHandler}
+          icon={"save"}
+        />
+      ),
+    });
+  }, [navigation, savePickedLocationHandler, initialLocation]);
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 export default Map;
 
 const styles = StyleSheet.create({
-    map: {
-        flex: 1,
-    },
+  map: {
+    flex: 1,
+  },
 });
