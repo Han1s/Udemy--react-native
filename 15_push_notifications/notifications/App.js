835bc56daadcb618c2654ed419d1f468eb4b5b9d
Jan Ranostaj <honzaranostaj@gmail.com>
2023-04-28 16:21:00 +0200

    253

--- 15_push_notifications/notifications/App.js
+++ 15_push_notifications/notifications/App.js
@@ -1,70 +1,100 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {StatusBar} from "expo-status-bar";
+import {Alert, Button, StyleSheet, View, Platform} from "react-native";
 import * as Notifications from "expo-notifications";
-import { useEffect } from "react";
+import {useEffect} from "react";
 
 Notifications.setNotificationHandler({
-  handleNotification: async () => {
-    return {
-      shouldPlaySound: false,
-      shouldSetBadge: false,
-      shouldShowAlert: true,
-    };
-  },
+    handleNotification: async () => {
+        return {
+            shouldPlaySound: false,
+            shouldSetBadge: false,
+            shouldShowAlert: true,
+        };
+    },
 });
 
 export default function App() {
-  useEffect(() => {
-    const subscription1 = Notifications.addNotificationReceivedListener(
-      (notification) => {
-        console.log('SUBSCRIPTION 1')
-        console.log(JSON.stringify(notification, null, 2));
+    useEffect(() => {
+        const configurePushNotifications = async () => {
+            const {status} = await Notifications.getPermissionsAsync();
+            let finalStatus = status;
+
+            if (finalStatus !== 'granted') {
+                const {status} = await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            }
+
+            if (finalStatus !== 'granted') {
+                Alert.alert('Permission required', 'Push notifications need the appropriate permissions.')
+                return;
+            }
+
+            const pushTokenData = await Notifications.getExpoPushTokenAsync()
+            console.log(pushTokenData);
+        }
+
+        configurePushNotifications().then()
+
+      if (Platform.OS === 'android') {
+        // Additional setup required for Android
+        Notifications.setNotificationChannelAsync('default', {
+          name: 'default',
+          importance: Notifications.AndroidImportance.DEFAULT
+        });
       }
-    );
+    }, [])
 
-    // When clicking on notification
-    const subscription2 = Notifications.addNotificationResponseReceivedListener((notification) => {
-        console.log('SUBSCRIPTION 2')
-        console.log(JSON.stringify(notification, null, 2));
-        const userName = notification.notification.request.content.data.userName;
-        console.log(userName);
-    });
+    useEffect(() => {
+        const subscription1 = Notifications.addNotificationReceivedListener(
+            (notification) => {
+                console.log('SUBSCRIPTION 1')
+                console.log(JSON.stringify(notification, null, 2));
+            }
+        );
 
-    return () => {
-      subscription1.remove();
-      subscription2.remove();
+        // When clicking on notification
+        const subscription2 = Notifications.addNotificationResponseReceivedListener((notification) => {
+            console.log('SUBSCRIPTION 2')
+            console.log(JSON.stringify(notification, null, 2));
+            const userName = notification.notification.request.content.data.userName;
+            console.log(userName);
+        });
+
+        return () => {
+            subscription1.remove();
+            subscription2.remove();
+        };
+    }, []);
+
+    const scheduleNotificationHandler = () => {
+        Notifications.scheduleNotificationAsync({
+            content: {
+                title: "My first local notification",
+                body: "This is the body of the notification",
+                data: {userName: "Max"},
+            },
+            trigger: {
+                seconds: 3,
+            },
+        });
     };
-  }, []);
 
-  const scheduleNotificationHandler = () => {
-    Notifications.scheduleNotificationAsync({
-      content: {
-        title: "My first local notification",
-        body: "This is the body of the notification",
-        data: { userName: "Max" },
-      },
-      trigger: {
-        seconds: 3,
-      },
-    });
-  };
-
-  return (
-    <View style={styles.container}>
-      <Button
-        title="Schedule Notification"
-        onPress={scheduleNotificationHandler}
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Button
+                title="Schedule Notification"
+                onPress={scheduleNotificationHandler}
+            />
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
